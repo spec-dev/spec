64d99014e76a509de4b8e46de795c91ad16b1a42
Ben Whittle <benwhittle31@gmail.com>
2022-09-04 23:05:59 -0500

    fix spec privilege grants

--- db/init/01-create-spec-schema.sql
+++ db/init/01-create-spec-schema.sql
@@ -1,13 +1,21 @@
--- Create spec schema and grant priveleges
+-- Create spec schema
 
-create schema if not exists spec;
+create schema if not exists spec AUTHORIZATION spec;
 
-grant usage on schema spec to spec;
+-- Give spec user access to the "spec" schema
+
+grant all privileges on all tables in schema spec to spec;
+grant all privileges on all sequences in schema spec to spec;
+grant all privileges on all functions in schema spec to spec;
 alter default privileges in schema spec grant all on tables to spec;
 alter default privileges in schema spec grant all on functions to spec;
 alter default privileges in schema spec grant all on sequences to spec;
 
-grant usage on schema public to spec;
+-- Give spec user access to the "public" schema
+
+grant all privileges on all tables in schema public to spec;
+grant all privileges on all sequences in schema public to spec;
+grant all privileges on all functions in schema public to spec;
 alter default privileges in schema public grant all on tables to spec;
 alter default privileges in schema public grant all on functions to spec;
 alter default privileges in schema public grant all on sequences to spec;
