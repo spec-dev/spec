60fb6f7e05251c9bd507d471c20c518e8eca123a
Ben Whittle <benwhittle31@gmail.com>
2022-09-04 23:11:13 -0500

    switch from raw queries in basic count ops

--- src/lib/db/ops.ts
+++ src/lib/db/ops.ts
@@ -1,19 +1,15 @@
 import { db } from './index'
 
 export async function doesSchemaExist(schema: string): Promise<boolean> {
-    const result = await db.raw(
-        'SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = ?', 
-        [schema],
-    )
-    return result.rowCount > 0
+    const result = await db.from('information_schema.schemata').where({ schema_name: schema }).count()
+    const count = result ? Number((result[0] || {}).count || 0) : 0
+    return count > 0
 }
 
 export async function doesTableExist(table: string, schema: string): Promise<boolean> {
-    const result = await db.raw(
-        'SELECT COUNT(*) FROM pg_tables WHERE schemaname = ? AND tablename = ?', 
-        [schema, table],
-    )
-    return result.rowCount > 0
+    const result = await db.from('pg_tables').where({ schemaname: schema, tablename: table }).count()
+    const count = result ? Number((result[0] || {}).count || 0) : 0
+    return count > 0
 }
 
 export async function areColumnsEmpty(tablePath: string, colNames: string[]): Promise<boolean> {
@@ -21,9 +17,8 @@ export async function areColumnsEmpty(tablePath: string, colNames: string[]): Pr
     for (let colName of colNames) {
         whereConditions[colName] = null
     }
-
     const result = await db.from(tablePath).whereNot(whereConditions).count()
-    const count = Number(result[0].count)
+    const count = result ? Number((result[0] || {}).count || 0) : 0
     return count === 0
 }
 
