0c1db7500f5598fdc07a564c20a1daa4cfba4087
Ben Whittle <benwhittle31@gmail.com>
2023-02-11 13:56:23 -0600

    remove debug

--- src/lib/services/ApplyDiffsService.ts
+++ src/lib/services/ApplyDiffsService.ts
@@ -499,10 +499,6 @@ class ApplyDiffsService {
             }
         }
 
-        if (this.linkTablePath === 'public.transactions') {
-            console.log(foreignTableQueryConditions)
-        }
-
         // Format record objects to upsert.
         const upsertRecords = []
         for (const diff of diffs) {
