4d689c9add2d38e8298a4c3bfa3d0afb641a2f37
Ben Whittle <benwhittle31@gmail.com>
2023-03-14 00:39:44 -0500

    add custom handler installment to local entrypoint.sh

--- bin/local/entrypoint.sh
+++ bin/local/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Install custom user-defined hooks if they exist.
+# Install custom user-defined handlers and hooks if they exist.
+[ -f ./.spec/handlers/package.json ] && npm install --omit=dev file:./.spec/handlers
 [ -f ./.spec/hooks/package.json ] && npm install --omit=dev file:./.spec/hooks
 
 npm start
