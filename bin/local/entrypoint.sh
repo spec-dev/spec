f8a767ed4ddbb37965d3e6b856445e4d235c675e
Ben Whittle <benwhittle31@gmail.com>
2023-04-25 15:34:23 -0500

    remove bin/

--- bin/local/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Install custom user-defined handlers and hooks if they exist.
-[ -f ./.spec/handlers/package.json ] && npm install --omit=dev file:./.spec/handlers
-[ -f ./.spec/hooks/package.json ] && npm install --omit=dev file:./.spec/hooks
-
-npm start
